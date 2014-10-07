f5a99681335218ed770e008394dbc16a8c84bdb1
Craig Thayer <cthayer@sazze.com>
2014-10-06 17:00:38 -0700

    version 0.0.2

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  coreTypes: require('./lib/core_types'),
+  enTypes: require('./lib/en_types'),
+  emitirTypes: require('./lib/emitir_types'),
+
+  SendService: require('./lib/SendService'),
+
+  SendClient: require('./lib/clients/SendClient')
+};
